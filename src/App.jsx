9afb436dec4fb7c30ac47677b835620c4abf878f
Erika Rostás <60824200+mazsola739@users.noreply.github.com>
2021-06-12 21:45:05 +0200

    finished visualization

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,44 @@
 import React from 'react';
 import plantumlEncoder from 'plantuml-encoder'
-//import logo from './logo.svg';
 import './App.css';
-import './rikaskill.js'
+import {
+  plsWork
+} from './rikaskill.js'
+import skills from './skills.json'
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { imgurl: `https://12ax7web.s3.amazonaws.com/accounts/1/products/imported/pew-pew-unicorn-t-shirt-teeturtle-1000x1000.jpg` };
+    const UMLVersion = plsWork(skills)
+    this.state = { jsonToUML: skills, UMLVersion, url: this.createURL(UMLVersion), };
   }
+  createURL = (UMLVersion) => {
+    var encoded = plantumlEncoder.encode(UMLVersion)
+    var url = 'http://www.plantuml.com/plantuml/img/' + encoded
+    return url
+  }
+
   onSubmit = (event) => {
     event.preventDefault()
-    const value = event.target[0].value
-    this.setState({ imgurl: value })
+    const value = JSON.parse(event.target[0].value)
+    const UMLVersion = plsWork(value)
+
+    this.setState({
+      jsonToUML: value,
+      UMLVersion,
+      url: this.createURL(UMLVersion)
+    })
   }
 
   render() {
+    console.log(this.state)
     return (
       <div className="App" >
-        <div>{JSON.stringify(this.state.imgurl)}</div><br />
+        <br />
         <div><form onSubmit={e => this.onSubmit(e)}><input type="text"></input><button type="submit">Submit</button></form></div><br />
-        <div><img alt="" src={this.state.imgurl}></img></div>
-        <div><p id="puppiduppi"></p></div>
-        <script src="./rikaskill.js"></script>
+        <div><img alt="" src={this.state.url}></img></div>
+        <pre>{JSON.stringify(this.state.jsonToUML)}</pre>
+        <pre>{`${this.state.UMLVersion}`}</pre>
       </div>
     )
   };
--- src/rikaskill.js
+++ src/rikaskill.js
@@ -1,84 +1,8 @@
-var skills = {
-    js: {
-        Frontend: {
-            frameworks: {
-                React: 0,
-                Angular: 0,
-                Vue: 0,
-                Svelte: 0
-            },
-            coreConcepts: {
-                syntax: 8,
-                insertJsIntoHtml: 7,
-                shorthands: 8,
-                hoisting: 7,
-                scope: 7,
-                closures: 3,
-                variables: 8,
-                math: 8,
-                strings: 8,
-                arrays: 8,
-                objects: 6,
-                dates: 6,
-                JSON: 5,
-                async: 2,
-                promises: 2,
-                events: 4,
-                callbackHell: 3,
-                prototypeInheritence: 4,
-                es6: 7,
-                es2016: 7,
-                es2017: 7,
-                es2018: 7,
-                es2019: 5,
-                es2020: 3,
-                es2021: 1
-            }
-        },
-        Backend: {
-            nodeJs: 0,
-            deno: 0,
-            webassembly: 0,
-            v8: 0,
-            chakra: 0,
-            spidermonkey: 0
-        }
-    },
-    css: {
-        preProcessors: {
-            sass: 7,
-            less: 0,
-            stylus: 0
-        },
-        coreConcepts: {
-            syntax: 8,
-            insertCssIntoHtml: 8,
-            selectors: 7,
-            unitsOfMeasurements: 6,
-            cascading: 4,
-            specificity: 5,
-            boxModel: 8,
-            css3Features: 7
-        }
-    },
-    html: {
-        coreConcepts: {
-            editors: 7,
-            syntax: 8,
-            HTMLforms: 8,
-            graphics: 5,
-            media: 5,
-            APIs: 3,
-            css3Features: 6
-        }
-    }
-};
-
 var baseColor = "#00abf5";
 
-function plsWork(QQ) {
-    let grrrrrrr = document.write(plantUMLing(result));
-    return grrrrrrr;
+export function plsWork(QQ) {
+    var result = getFormatted(QQ, "");
+    return plantUMLing(result);
 }
 
 //creating the data's into the shape of needing to plantUML
@@ -122,7 +46,7 @@ function coloring(string) {
     return string.map((el) => {
         if (!el.includes("_") && el.length > 0) {
             var lightness = lighting(el)
-            return el.replace("* ", `* [${shade(baseColor, lightness)}]`)
+            return el.replace("* ", `*[${shade(baseColor, lightness)}] `)
         } else {
             return el
         }
@@ -147,7 +71,7 @@ function color(red, green, blue) {
 //how much lightening?
 function lighting(string) {
     var howManyStars = string.match(/[*]/g).length;
-    return (howManyStars * 2) / 10;
+    return (howManyStars * 1) / 10;
 }
 
 //getting lightness of the color
@@ -167,10 +91,10 @@ function shade(col, light) {
     return color(r, g, b);
 }
 
-var result = getFormatted(skills, "");
 
-plsWork(skills);
+
+//plsWork(skills);
 
 //console.log("Rika");
 //console.log(shade(baseColor, 0.2));
-console.log(plantUMLing(result));
+//console.log(plantUMLing(result));
