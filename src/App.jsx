126d3770a0586be342f9eadb96170be31aabb33e
Erika Rostás <60824200+mazsola739@users.noreply.github.com>
2021-06-13 11:46:00 +0200

    feat: new json visualization

--- src/App.jsx
+++ src/App.jsx
@@ -2,46 +2,46 @@ import React, { useState } from 'react';
 import plantumlEncoder from 'plantuml-encoder'
 import './App.css';
 import {
-  showToMe
+  showToMeOnSite
 } from './rikaskill.js'
 import {
-  plsWork
+  showToMeInPic
 } from './rikaskill.js'
 import skills from './skills.json'
 
-const createURL = (UMLVersion) => {
-  var encoded = plantumlEncoder.encode(UMLVersion)
-  var url = 'http://www.plantuml.com/plantuml/img/' + encoded
-  return url
+const createUrl = (umlVersion) => {
+  var encoded = plantumlEncoder.encode(umlVersion)
+  var imgUrl = 'http://www.plantuml.com/plantuml/img/' + encoded
+  return imgUrl
 }
 
 const App = (props) => {
 
-  const [jsonToUML, setJsonToUML] = useState(skills)
-  const [showTheJson, setShowTheJson] = useState(showToMe(skills))
-  const [umlVersion, setUmlVersion] = useState(plsWork(skills))
-  const [url, setUrl] = useState(createURL(plsWork(skills)))
+  const [jsonToUml, setJsonToUml] = useState(skills)
+  const [showTheJson, setShowTheJson] = useState(showToMeOnSite(skills))
+  const [umlVersion, setUmlVersion] = useState(showToMeInPic(skills))
+  const [imgUrl, setImgUrl] = useState(createUrl(showToMeInPic(skills)))
 
   //todo szerkeszteni
   const onSubmit = (event) => {
     event.preventDefault()
     const value = JSON.parse(event.target[0].value)
-    const umlVersion = plsWork(value)
+    const showTheJson = showToMeOnSite(value)
+    const umlVersion = showToMeInPic(value)
 
-    setJsonToUML(value)
-    setShowTheJson(value)
+    setJsonToUml(value)
+    setShowTheJson(showTheJson)
     setUmlVersion(umlVersion)
-    setUrl(createURL(umlVersion))
+    setImgUrl(createUrl(umlVersion))
   }
 
-  //console.log(this.state)
   return (
     <div className="App" >
       <br />
       <div><form onSubmit={e => onSubmit(e)}><input type="text"></input><button type="submit">Submit</button></form></div><br />
-      <div><img alt="" src={url}></img></div>
+      <div><img alt="" src={imgUrl}></img></div>
       {/* debug todo blueprint marci dobta a linket diszkordon https://blueprintjs.com/docs/#core/components/collapse --save*/}
-      <pre>{JSON.stringify(jsonToUML)}</pre>
+      <pre>{JSON.stringify(jsonToUml)}</pre>
       <pre>{`${showTheJson}`}</pre>
       <pre>{`${umlVersion}`}</pre>
     </div>
--- src/rikaskill.js
+++ src/rikaskill.js
@@ -1,26 +1,28 @@
-//todo optimalization
-
 var baseColor = "#00abf5";
 
-export function plsWork(QQ) {
-    var result = getFormatted(QQ, "");
-    return plantUMLing(result);
+export function showToMeInPic(json) {
+    return givingPlantUmlSetting(getFormattedToUml(json, ""));
+}
+export function showToMeOnSite(json) {
+    return getVisualizedToSite(json, "");
 }
 
-export function showToMe(QQ) {
-    var result = getVisualizedToSite(QQ, "");
-    return result;
+const getFormattedToUml = (object, prefix) => {
+    var result = "";
+    Object.entries(object).forEach(([key, value], index, { length }) => {
+        result += prefix + (index + 1 === length ? "* " : "* ") + key;
+        if (value && isNode(value)) {
+            result += "\n";
+            result += getFormattedToUml(value, prefix + (index + 1 === length ? "*" : "*"));
+        } else {
+            if (value || value === 0) result += ": " + value;
+            result += "\n";
+        }
+    });
+    return unBoxing(result);
 }
 
-const isLeaf = function (x) {
-    return typeof x === "string"
-}
-
-const isNode = function (x) {
-    return typeof x === "object"
-}
-
-const getVisualizedToSite = function (object, prefix) {
+const getVisualizedToSite = (object, prefix) => {
     var result = "";
     Object.entries(object).forEach(([key, value], index, { length }) => {
         result += prefix + (index + 1 === length ? "└─ " : "├─ ") + key;
@@ -35,57 +37,43 @@ const getVisualizedToSite = function (object, prefix) {
     return result;
 }
 
-//creating the data's into the shape of needing to plantUML
-//todo isleaf isnode
 
-const getFormatted = function (object, prefix) {
-    var result = "";
-    Object.entries(object).forEach(([key, value], index, { length }) => {
-        result += prefix + (index + 1 === length ? "* " : "* ") + key;
-        if (value && typeof value === "object") {
-            result += "\n";
-            result += getFormatted(value, prefix + (index + 1 === length ? "*" : "*"));
-        } else {
-            if (value || value === 0) result += ": " + value;
-            result += "\n";
-        }
-    });
-    return unBoxing(result);
+const isLeaf = (value) => {
+    return typeof value === "string"
 }
-//does the children got number?
-const hasNumber = function (string) {
+const isNode = (value) => {
+    return typeof value === "object"
+}
+const hasNumber = (string) => {
     return /\d/.test(string);
 }
-//leafs "unboxing"
-const unBoxing = function (string) {
+
+const unBoxing = (string) => {
     return string
         .split("\n")
-        .map((el) => (hasNumber(el) ? el.replace("* ", "*_ ") : el))
+        .map((el) => (hasNumber(el) && isLeaf(el) ? el.replace("* ", "*_ ") : el))
         .join("\n");
 }
-//adding plantUML settings
-const plantUMLing = function (string) {
+const givingPlantUmlSetting = (string) => {
     string = string.split("\n");
-    string = coloring(string);
+    string = coloringTheBox(string);
     string[0] = "@startmindmap\n!theme spacelab\ntitle Rika's Skill-visualizer\n\n" + string[0];
     string[string.length - 1] = "\nlegend right\n  Rika©\nendlegend\n@endmindmap";
     return string.join("\n");
 }
 
-//adding color to the boxes
-const coloring = function (string) {
+
+const coloringTheBox = (string) => {
     return string.map((el) => {
         if (!el.includes("_") && el.length > 0) {
-            var lightness = lighting(el)
-            return el.replace("* ", `*[${shade(baseColor, lightness)}] `)
+            var lightness = lightingWithDepth(el)
+            return el.replace("* ", `*[${shadeOfColor(baseColor, lightness)}] `)
         } else {
             return el
         }
     });
 }
-
-//converting colors to hexadecimal
-const rgbToHex = function (color) {
+const rgbToHex = (color) => {
     color = Math.round(color);
     if (color < 0) color = 0;
     if (color > 255) color = 255;
@@ -93,31 +81,25 @@ const rgbToHex = function (color) {
     if (string.length < 2) string = "0" + string;
     return string;
 }
-
-//hexadecimal form of colors
-const color = function (red, green, blue) {
+const colorHexForm = (red, green, blue) => {
     return "#" + rgbToHex(red) + rgbToHex(green) + rgbToHex(blue);
 }
-
-//how much lightening?
-const lighting = function (string) {
+const lightingWithDepth = (string) => {
     var howManyStars = string.match(/[*]/g).length;
     return howManyStars / 10;
 }
-
-//getting lightness of the color
-const shade = function (col, light) {
-    var r = parseInt(col.substr(1, 2), 16);
-    var g = parseInt(col.substr(3, 2), 16);
-    var b = parseInt(col.substr(5, 2), 16);
+const shadeOfColor = (color, light) => {
+    var red = parseInt(color.substr(1, 2), 16);
+    var green = parseInt(color.substr(3, 2), 16);
+    var blue = parseInt(color.substr(5, 2), 16);
     if (light < 0) {
-        r = (1 + light) * r;
-        g = (1 + light) * g;
-        b = (1 + light) * b;
+        red = (1 + light) * red;
+        green = (1 + light) * green;
+        blue = (1 + light) * blue;
     } else {
-        r = (1 - light) * r + light * 255;
-        g = (1 - light) * g + light * 255;
-        b = (1 - light) * b + light * 255;
+        red = (1 - light) * red + light * 255;
+        green = (1 - light) * green + light * 255;
+        blue = (1 - light) * blue + light * 255;
     }
-    return color(r, g, b);
+    return colorHexForm(red, green, blue);
 }
